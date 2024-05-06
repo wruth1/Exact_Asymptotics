7c9d48fe087af90f44b937440d51be596cac8f2a
William Ruth <wruth1@users.noreply.github.com>
2024-05-06 15:09:56 -0700

    Implement binary, binary, fixed in R. SEs are currently wrong

--- src/Helpers.R
+++ src/Helpers.R
@@ -109,3 +109,146 @@ d_gamma_d_theta <- function(eta, x, W, a, b) {
 
   return(c(dg_da_0, dg_da_1, dg_dA_2, dg_db_0, dg_db_1, dg_db_2, dg_dB_3))
 }
+
+
+
+
+
+
+
+######################################################
+#### Binary Response, Binary Mediator, Fixed-Effs ####
+######################################################
+
+
+# Compute mediation effect
+
+## Probability of Y=1 given inputs
+PY1 <- function(eta, zeta, beta) {
+  Q1 <- 1 + exp(-zeta)
+  Q2 <- 1 + exp(-eta)
+  Q3 <- 1 + exp(-zeta - beta)
+
+  num <- Q1 + (Q2 - 1) * Q3
+  den <- Q1 * Q2 * Q3
+
+  return(num / den)
+}
+
+## Probability of Y=0 given inputs
+PY0 <- function(eta, zeta, beta) {
+  Q1 <- exp(-zeta)
+  Q2 <- exp(-eta)
+  Q3 <- exp(-beta)
+
+  num <- Q1 * (Q2 + Q3 + Q1 * Q3 + Q1 * Q2 * Q3)
+  den <- (1 + Q1) * (1 + Q2) * (1 + Q1 * Q3)
+
+  return(num / den)
+}
+
+
+get_odds_ref <- function(eta, zeta, beta) {
+  num <- PY1(eta, zeta, beta)
+  den <- PY0(eta, zeta, beta)
+
+  return(num / den)
+}
+
+get_odds <- function(eta, zeta, beta) {
+  Q1 <- exp(-zeta)
+  Q1_inv <- 1 / Q1
+  Q2 <- exp(-eta)
+  Q3 <- exp(-beta)
+
+  num <- 1 + Q1_inv + Q2 * (Q3 + Q1_inv)
+  den <- Q2 + Q3 + Q1 * Q3 + Q1 * Q2 * Q3
+
+  return(num / den)
+}
+
+get_odds_ratio <- function(eta, a_x, zeta, b_x, b_m) {
+  odds_1 <- get_odds(eta + a_x, zeta + b_x, b_m)
+  odds_2 <- get_odds(eta, zeta, b_m)
+  OR_hat <- odds_1 / odds_2
+  return(OR_hat)
+}
+
+
+
+
+
+
+# Gradients of OR, obtained from Maple
+# I verified all of these against a simple finite difference calculation
+
+d_OR_d_eta <- function(eta, a_x, zeta, b_x, b_m) {
+  -exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m)) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) - (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) ^ 2 / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-eta - a_x) - exp(-zeta - b_x - eta - a_x - b_m)) + (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) ^ 2 * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * exp(-eta) * (1 + exp(-zeta - b_m)) + (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (-exp(-eta) - exp(-zeta - eta - b_m))
+}
+
+d_OR_d_a_x <- function(eta, a_x, zeta, b_x, b_m) {
+  -exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m)) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) - (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) ^ 2 / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-eta - a_x) - exp(-zeta - b_x - eta - a_x - b_m))
+}
+
+d_OR_d_zeta <- function(eta, a_x, zeta, b_x, b_m) {
+  (-exp(-zeta - b_x) - exp(-eta - a_x) * exp(-zeta - b_x - b_m)) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) - (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) ^ 2 / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-zeta - b_x - b_m) - exp(-zeta - b_x - eta - a_x - b_m)) - (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) ^ 2 * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-zeta) - exp(-eta) * exp(-zeta - b_m)) + (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (-exp(-zeta - b_m) - exp(-zeta - eta - b_m))
+}
+
+
+
+d_OR_d_b_x <- function(eta, a_x, zeta, b_x, b_m) {
+  (-exp(-zeta - b_x) - exp(-eta - a_x) * exp(-zeta - b_x - b_m)) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) +
+    (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) -
+    (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) ^ 2 / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-zeta - b_x - b_m) - exp(-zeta - b_x - eta - a_x - b_m))
+}
+
+d_OR_d_b_m <- function(eta, a_x, zeta, b_x, b_m) {
+  -exp(-eta - a_x) * exp(-zeta - b_x - b_m) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) -
+    (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) ^ 2 / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * (-exp(-b_m) - exp(-zeta - b_x - b_m) - exp(-zeta - b_x - eta - a_x - b_m)) +
+    (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) ^ 2 * exp(-zeta) * (exp(-eta) + exp(-b_m) + exp(-zeta - b_m) + exp(-zeta - eta - b_m)) * exp(-eta) * exp(-zeta - b_m) +
+    (1 + exp(-zeta - b_x) + exp(-eta - a_x) * (1 + exp(-zeta - b_x - b_m))) / exp(-zeta - b_x) / (exp(-eta - a_x) + exp(-b_m) + exp(-zeta - b_x - b_m) + exp(-zeta - b_x - eta - a_x - b_m)) / (1 + exp(-zeta) + exp(-eta) * (1 + exp(-zeta - b_m))) * exp(-zeta) * (-exp(-b_m) - exp(-zeta - b_m) - exp(-zeta - eta - b_m))
+}
+
+
+
+
+
+# Build gradient wrt reg pars
+d_OR_d_a0 <- function(eta, a_x, zeta, b_x, b_m) {
+  return(d_OR_d_eta(eta, a_x, zeta, b_x, b_m))
+}
+
+d_OR_d_a1 <- function(eta, a_x, zeta, b_x, b_m, x_ref) {
+  return(x_ref * d_OR_d_eta(eta, a_x, zeta, b_x, b_m) + d_OR_d_a_x(eta, a_x, zeta, b_x, b_m))
+}
+
+d_OR_d_A2 <- function(eta, a_x, zeta, b_x, b_m, W_ref) {
+  return(W_ref * c(d_OR_d_eta(eta, a_x, zeta, b_x, b_m)))
+}
+
+d_OR_d_b0 <- function(eta, a_x, zeta, b_x, b_m) {
+  return(d_OR_d_zeta(eta, a_x, zeta, b_x, b_m))
+}
+
+d_OR_d_b1 <- function(eta, a_x, zeta, b_x, b_m) {
+  return(d_OR_d_b_m(eta, a_x, zeta, b_x, b_m))
+}
+
+d_OR_d_b2 <- function(eta, a_x, zeta, b_x, b_m, x_ref) {
+  return(x_ref * d_OR_d_zeta(eta, a_x, zeta, b_x, b_m) + d_OR_d_b_x(eta, a_x, zeta, b_x, b_m))
+}
+
+d_OR_d_B3 <- function(eta, a_x, zeta, b_x, b_m, W_ref) {
+  return(W_ref * c(d_OR_d_zeta(eta, a_x, zeta, b_x, b_m)))
+}
+
+d_OR_d_theta <- function(eta, a_x, zeta, b_x, b_m, x_ref, W_ref) {
+  return(c(d_OR_d_a0(eta, a_x, zeta, b_x, b_m),
+           d_OR_d_a1(eta, a_x, zeta, b_x, b_m, x_ref),
+           d_OR_d_A2(eta, a_x, zeta, b_x, b_m, W_ref),
+           d_OR_d_b0(eta, a_x, zeta, b_x, b_m),
+           d_OR_d_b1(eta, a_x, zeta, b_x, b_m),
+           d_OR_d_b2(eta, a_x, zeta, b_x, b_m, x_ref),
+           d_OR_d_B3(eta, a_x, zeta, b_x, b_m, W_ref)))
+}
+
